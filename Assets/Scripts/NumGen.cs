502970317fcd16b93dee8813de1e0638f3d5e4c6
Miguel239g <127420871+Elinmaculado@users.noreply.github.com>
2024-10-06 18:05:35 -0600

    Initial commit

---
+++ Assets/Scripts/NumGen.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NumGen : MonoBehaviour
+{
+    public TMP_Text slotNum;
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DelayNum();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slotNum.text = Random.Range(0, 9).ToString();
+
+    }
+    public void offEnabled()
+    {
+        Invoke("DelayNum", time);
+    }
+
+    public void OnEnable()
+    {
+        enabled = true;
+    }
+
+    void DelayNum()
+    {
+        enabled = false;
+    }
+}
---
+++ Assets/Scripts/Result.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;  // Para usar TextMeshPro
+
+public class Result : MonoBehaviour
+{
+    public TMP_Text[] numberTexts;  // Referencia a los textos que contienen los números generados
+    public TMP_Text resultText;     // Texto que mostrará el resultado
+
+    // Método que cuenta cuántas veces se repite el número que más veces aparece
+    void Update()
+    {
+        Dictionary<int, int> numberCount = new Dictionary<int, int>();
+        int[] numbers = new int[numberTexts.Length];
+
+        // Obtener los números de los textos
+        for (int i = 0; i < numberTexts.Length; i++)
+        {
+            // Convertimos el texto del TMP_Text a número entero
+            numbers[i] = int.Parse(numberTexts[i].text);
+        }
+
+        // Cuenta las veces que aparece cada número
+        foreach (int number in numbers)
+        {
+            if (numberCount.ContainsKey(number))
+            {
+                numberCount[number]++;
+            }
+            else
+            {
+                numberCount[number] = 1;
+            }
+        }
+
+        // Encuentra el número con más repeticiones
+        int maxRepetitions = 0;
+        foreach (var pair in numberCount)
+        {
+            if (pair.Value > maxRepetitions)
+            {
+                maxRepetitions = pair.Value;
+            }
+        }
+
+        // Muestra el resultado (cantidad de veces que se repite el número más repetido)
+        if (maxRepetitions <= 1)
+        {
+            resultText.text = "Máx. repeticiones: 0";
+        }
+        else
+        {
+            resultText.text = "Máx. repeticiones: " + maxRepetitions;
+
+        }
+    }
+}
